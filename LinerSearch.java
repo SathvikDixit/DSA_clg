c1f419903b30d4ba1acc5a175c0609c9af812baf
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-22 15:17:55 +0530

    aoisupqor

---
+++ LinerSearch.java
@@ -0,0 +1,11 @@
+public class LinerSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 4) {
+                System.out.println("the element 4 is present in index "+i);
+            }
+        }
+    }
+}
