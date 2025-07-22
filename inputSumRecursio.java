0f430d095eff27b0b0298a84f0f53023cf5b78ee
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-22 10:11:40 +0530

    qiuweprouasf

--- inputSumRecursio.java
+++ inputSumRecursio.java
@@ -1,12 +1,13 @@
 public class inputSumRecursio {
     public static void main(String[] args) {
-        int n = 6996;
+        int n = 16996;
         int ans = rec (n);
         System.out.println(ans);
     }
+
     public static int rec (int n) {
         if (n <= 0) return 0;
-        int sum = n%10;
-        return sum + rec(n/10);
+        int sum = n%10;    // n % 100 for two digits
+        return sum + rec(n/10);   // n/100 for two digits
     }
 }
