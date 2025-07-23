e5ad98c43859a7e0689f95034373491bb7a2960b
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-23 10:37:55 +0530

    finding min and max value in an array  DONE

--- findMaxAndMin.java
+++ findMaxAndMin.java
@@ -4,12 +4,16 @@ public class findMaxAndMin {
         maxMin(arr);
     }
     public static void maxMin (int[] arr) {
-        int n = arr.length;
+        // int n = arr.length;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            if(arr[i] > max) max = arr[i];
-            if(arr[i] < min) min = arr[i];
+        // for (int i = 0; i < n; i++) {
+        //     if(arr[i] > max) max = arr[i];
+        //     if(arr[i] < min) min = arr[i];
+        // }
+        for(int i : arr) {
+            if(i > max) max = i;
+            if(i < min) min = i;
         }
         System.out.println("Maximum value is "+max);
         System.out.println("Minimum value is "+min);
