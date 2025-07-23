bdecfa28797d8a8e8b52862f47f14b1c09bd3830
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-23 11:54:51 +0530

    REversing an array

---
+++ reversingArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class reversingArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7}; 
+        int[] ans = revArr(arr1);
+        System.out.println("" + Arrays.toString(ans));
+    }
+
+    public static int[] revArr(int[] arr1) {
+        int n = arr1.length, i, j = n -1;
+        for (i = 0; i < n/2; i++) {
+            swap(arr1, i, j);
+            j--;
+        }
+        return arr1;
+    }
+
+    public static void swap(int[] arr1, int i, int j) {
+        int temp = arr1[j];
+        arr1[j] = arr1[i];
+        arr1[i] = temp;
+    }
+}
