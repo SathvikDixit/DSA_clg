f1c876efc026a10b43647f1d36a059256c504073
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-23 15:00:27 +0530

    Done Bubble sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class bubbleSort {
     public static void main(String[] args) {
@@ -8,13 +9,17 @@ public class bubbleSort {
         int n = arr.length, i;
         int j;
         for (i = 0; i < n-1; i++) {
+            System.out.println("The "+(i+1)+" iteration is: "+Arrays.toString(arr));
+            int swapped = 0;
             for(j = 0; j < n-i-1; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = 1;
                 }
             }
+            if (swapped == 0) break;
         }
         for(int a = 0; a < n; a++) {
             System.out.print(arr[a]+" ");
--- selectionSort.java
+++ selectionSort.java
@@ -10,7 +10,7 @@ public class selectionSort {
     }
 
     public static int[] sort(int arr[], int n) {
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 0; i < n-1; i++) {
             int mini = i;
             for (int j = i+1; j < n; j++) {
                 if(arr[j] < arr[mini]) mini = j;
