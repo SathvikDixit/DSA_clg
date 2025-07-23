a9d67a53b02ef82379fb0a452fbc46ae028208bc
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-23 15:50:48 +0530

    modified selection sort

--- selectionSort.java
+++ selectionSort.java
@@ -2,25 +2,29 @@ import java.util.Arrays;
 
 public class selectionSort {
     public static void main(String[] args) {
-        int[] arr = {95, 88, 12, 1, 22, 69, 96};
+        int[] arr = { 95, 88, 12, 1, 22, 69, 96 };
         int n = arr.length;
-        System.out.println("array before sorting "+Arrays.toString(arr));
+        System.out.println("array before sorting " + Arrays.toString(arr));
         int[] ans = sort(arr, n);
-        System.out.println("after sorting : "+Arrays.toString(ans));
+        System.out.println("after sorting : " + Arrays.toString(ans));
     }
 
     public static int[] sort(int arr[], int n) {
-        for(int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int mini = i;
-            for (int j = i+1; j < n; j++) {
-                if(arr[j] < arr[mini]) mini = j;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini])
+                    mini = j;
             }
-            int temp = arr[mini];
-            arr[mini] = arr[i];
-            arr[i] = temp;
+            if (mini != i) {
+                int temp = arr[mini];
+                arr[mini] = arr[i];
+                arr[i] = temp;
+            }
+
         }
         // for(int i = 0; i<n; i++) {
-        //     System.out.println("Array after sorting "+arr[i]);
+        // System.out.println("Array after sorting "+arr[i]);
         // }
         // System.out.println();
         return arr;
