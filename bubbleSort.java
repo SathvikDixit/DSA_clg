b0b150d3a8a9d220e210df143a88d7428845a44a
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-23 14:30:49 +0530

    uwwrouwuoreuou

---
+++ bubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 2, 7, 0, 7, 5, 2, 6};
+        bubble(arr);
+    }
+    public static void bubble(int[] arr) {
+        int n = arr.length, i;
+        int j;
+        for (i = 0; i < n; i++) {
+            for(j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int a = 0; a < n; a++) {
+            System.out.print(arr[a]+" ");
+        }
+    }
+}
