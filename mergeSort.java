88118064b892adf02c3082c23e1cb9c3cf804add
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-24 10:52:18 +0530

    this problem is from hackerrank CTS technical problems

---
+++ mergeSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class mergeSort {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 9, 4, 0, 3, 8, 2};
+        System.out.println("Array before sorting: "+Arrays.toString(arr));
+        
+    }
+
+    public static void Merge(int[] arr) {
+        int n = arr.length;
+
+
+    }
+}
---
+++ problem1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number the array elements to be printed and x value: ");
+        int arrSize = sc.nextInt();
+        int x = sc.nextInt();
+        int sum = 0;
+        int[] arr1 = new int[arrSize];
+        int[] arr2 = new int[arrSize];
+        
+        for (int i = 0; i < arrSize; i++) {
+            arr1[i] = i + 1;
+        }
+
+        for (int i = 0; i < arrSize-1; i++) {
+            arr2[i] = arr1[i] + arr1[i+1];
+        }
+        arr2[arrSize-1] = arr1[0] + arr1[arrSize-1];
+
+        // System.out.println(Arrays.toString(arr1));
+        // System.out.println(Arrays.toString(arr2));
+
+        for(int i = 0; i < arrSize; i++) {
+            if(arr2[i]%x == 0) {
+                sum += arr2[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
